fcd0d70127127604c49f56a283ff5912bace65a3
Mike <lanselot777hailander@gmail.com>
2023-07-19 18:03:00 -0400

    first commit

---
+++ msvc-usuarios/src/main/java/org/mgutty/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -0,0 +1,87 @@
+package org.mgutty.springcloud.msvc.usuarios.controllers;
+
+import jakarta.validation.Valid;
+import org.mgutty.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.mgutty.springcloud.msvc.usuarios.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detalle(@PathVariable(name = "id") Long id) {
+        Optional<Usuario> usuarioOptional = service.porId(id);
+        if (usuarioOptional.isPresent()) {
+            return ResponseEntity.ok().body(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> crear(@Valid @RequestBody Usuario usuario, BindingResult result) {
+        if(service.porEmail(usuario.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest()
+                    .body(Collections
+                            .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+        }
+        if(result.hasErrors()) {
+            return validar(result);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@Valid @RequestBody Usuario usuario, BindingResult result, @PathVariable Long id) {
+        if(result.hasErrors()) {
+            return validar(result);
+        }
+        Optional<Usuario> o = service.porId(id);
+        if (o.isPresent()){
+            Usuario usuarioDB = o.get();
+            if(!usuario.getEmail().equalsIgnoreCase(usuarioDB.getEmail()) && service.porEmail(usuario.getEmail()).isPresent()) {
+                return ResponseEntity.badRequest()
+                        .body(Collections
+                                .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+            }
+
+            usuarioDB.setNombre(usuario.getNombre());
+            usuarioDB.setEmail(usuario.getEmail());
+            usuarioDB.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDB));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        Optional<Usuario> o = service.porId(id);
+        if (o.isPresent()) {
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    private static ResponseEntity<Map<String, String>> validar(BindingResult result) {
+        Map<String, String> errores = new HashMap<>();
+        result.getFieldErrors().forEach(err -> {
+            errores.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
+    }
+}
---
+++ msvc-usuarios/src/main/java/org/mgutty/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package org.mgutty.springcloud.msvc.usuarios.repositories;
+
+import org.mgutty.springcloud.msvc.usuarios.models.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
